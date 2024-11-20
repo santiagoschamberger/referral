46b75151e671c37fe5fe02158048ef0459b72195
Mohit Sharma <mohi@mediahubster.com>
2024-11-21 00:03:14 +0530

    envup date

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'http://44.210.39.119:3000/api/v1',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
